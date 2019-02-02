fba1fc4bd4b5c67b4cbee28f5454caa18aaedc76
Sitnikov Roman <dev.sitnikov@gmail.com>
2019-02-02 22:54:47 +0300

    Start project

---
+++ android/src/ru/ondigit/bird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.ondigit.bird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.ondigit.bird.FlappeeBeeGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappeeBeeGame(), config);
+	}
+}
